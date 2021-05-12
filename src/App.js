033d361224de3634c08c5edc0494b6a5dc40b009
Snehangshu Biswas <dev.snehangshu@gmail.com>
2021-05-12 09:48:53 +0530

    app finished

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import ExpenseItem from './components/ExpenseItem.js'
+import { useState } from 'react'
+import Expenses from './components/Expenses/Expenses'
+import NewExpense from './components/NewExpense/NewExpense'
+
+const DUMMY_EXPENSES = [
+	{
+		title: 'Car Insurance',
+		amount: 294.64,
+		date: new Date(2019, 2, 28),
+		id: 'e1'
+	},
+	{ title: 'New TV', amount: 799.95, date: new Date(2021, 2, 28), id: 'e2' },
+	{
+		title: 'Toilet paper',
+		amount: 94.64,
+		date: new Date(2021, 2, 28),
+		id: 'e3'
+	},
+	{ title: 'Grocery', amount: 164.44, date: new Date(2020, 2, 28), id: 'e4' }
+]
 
 function App() {
+	const [expenses, setExpenses] = useState(DUMMY_EXPENSES)
+	const addExpenseHandler = expense => {
+		setExpenses(prevExp => [expense, ...prevExp])
+	}
+
 	return (
 		<div>
-			<h2>Let's get started</h2>
-			<p>This is also visible</p>
-			<ExpenseItem />
+			<NewExpense onAddExpense={addExpenseHandler} />
+			<Expenses items={expenses} />
 		</div>
 	)
 }
